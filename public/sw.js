f4fe5083f5eb9bf462c15d23a9f100caa308b0e4
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-05-12 18:35:57 +0300

    Implement routing specific strategies

--- public/sw.js
+++ public/sw.js
@@ -1,20 +1,21 @@
 var CACHE_STATIC_NAME = "static-v2";
 var CACHE_DYNAMIC_NAME = "dynamic-v1";
+var STATIC_FILES = [
+	"/",
+	"/index.html",
+	"/src/css/app.css",
+	"/src/css/main.css",
+	"/src/js/main.js",
+	"/src/js/material.min.js",
+	"https://fonts.googleapis.com/css?family=Roboto:400,700",
+	"https://fonts.googleapis.com/icon?family=Material+Icons",
+	"https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css"
+];
 
 self.addEventListener("install", function(event) {
 	event.waitUntil(
 		caches.open(CACHE_STATIC_NAME).then(function(cache) {
-			cache.addAll([
-				"/",
-				"/index.html",
-				"/src/css/app.css",
-				"/src/css/main.css",
-				"/src/js/main.js",
-				"/src/js/material.min.js",
-				"https://fonts.googleapis.com/css?family=Roboto:400,700",
-				"https://fonts.googleapis.com/icon?family=Material+Icons",
-				"https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css"
-			]);
+			cache.addAll(STATIC_FILES);
 		})
 	);
 });
@@ -43,13 +44,55 @@ self.addEventListener("activate", function(event) {
 // });
 
 // Dynamic caching for cache then network strategy
+// self.addEventListener("fetch", function(event) {
+// 	event.respondWith(
+// 		caches.open(CACHE_DYNAMIC_NAME).then(function(cache) {
+// 			return fetch(event.request).then(function(response) {
+// 				cache.put(event.request.url, response.clone());
+// 				return response;
+// 			});
+// 		})
+// 	);
+// });
+
+function isInArray(string, array) {
+	for (var i = 0; i < array.length; i++) {
+		if (array[i] === string) {
+			return true;
+		}
+		return false;
+	}
+}
+
+// Add routing specific strategies
 self.addEventListener("fetch", function(event) {
-	event.respondWith(
-		caches.open(CACHE_DYNAMIC_NAME).then(function(cache) {
-			return fetch(event.request).then(function(response) {
-				cache.put(event.request.url, response.clone());
-				return response;
-			});
-		})
-	);
+	if (event.request.url.indexOf("https://httpbin.org/ip") > -1) {
+		event.respondWith(
+			caches.open(CACHE_DYNAMIC_NAME).then(function(cache) {
+				return fetch(event.request).then(function(response) {
+					cache.put(event.request.url, response.clone());
+					return response;
+				});
+			})
+		);
+	} else if (isInArray(event.request.url, STATIC_FILES)) {
+		event.respondWith(caches.match(event.request));
+	} else {
+		event.respondWith(
+			caches.match(event.request).then(function(response) {
+				if (response) {
+					return response;
+				} else {
+					return fetch(event.request)
+						.then(function(res) {
+							caches.open(CACHE_DYNAMIC_NAME).then(function(cache) {
+								cache.put(event.request.url, res.clone());
+								return res;
+							});
+						})
+						.catch(function(err) {});
+				}
+			})
+		);
+	}
 });
